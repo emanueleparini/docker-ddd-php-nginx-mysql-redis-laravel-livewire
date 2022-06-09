392680d246a1268ab092889924c100bcf4678925
Emanuele Parini <102211751+emanueleparini@users.noreply.github.com>
2022-06-09 16:50:43 +0200

    Add files via upload

---
+++ app/public/index.php
@@ -0,0 +1,7 @@
+<?php phpinfo()?>
+
+<?php
+// $pdo = new PDO('mysql:dbname=demo_db;host=mysql','root','root',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+// $query = $pdo->query('SHOW VARIABLES like "version"');
+// $row = $query->fetch();
+// echo 'Mysql version: ' . $row['Value'];
